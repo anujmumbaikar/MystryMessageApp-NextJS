4d675d7c93b8bdf84bcef9108d3ebd7a3dbd17cd
Anuj Mumbaikar <anujmumbaikar120405@gmail.com>
2025-04-15 23:21:21 +0530

    Implement MongoDB connection utility with error handling

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+const connection: ConnectionObject = {};
+async function dbConnect():Promise<void>{
+    if(connection.isConnected){
+        console.log("Already connected to MongoDB");
+        return;
+    }
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI || "")
+        connection.isConnected = connectionInstance.connections[0].readyState;
+        console.log("Connected to MongoDB");
+        
+    } catch (error) {
+        console.log("Database connection error:", error);
+        process.exit(1);
+    }
+}
+export default dbConnect;
