ac14c46b9f938fdddcd875fea37f7066d4917f5a
Anuj Mumbaikar <anujmumbaikar120405@gmail.com>
2025-04-16 17:52:36 +0530

    fix: correct username field to email in credentials provider and ensure proper redirection in middleware

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -11,7 +11,7 @@ export const authOptions: NextAuthOptions = {
             id:"credentials",
             name:"Credentials",
             credentials:{
-                username: { label: "Email", type: "text"},
+                email: { label: "Email", type: "text"},
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials:any):Promise<any>{
