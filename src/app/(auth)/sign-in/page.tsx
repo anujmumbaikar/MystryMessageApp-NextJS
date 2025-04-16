cdc602cdbab83f7f6c29921e68270971cac8d460
Anuj Mumbaikar <anujmumbaikar120405@gmail.com>
2025-04-16 17:44:23 +0530

    feat: refactor authentication components and implement AuthProvider for session management

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-function page() {
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>page</div>
+    <>
+      Not signed in <br />
+      <button className="bg-orange-500" onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default page
--- src/middleware.ts
+++ src/middleware.ts
@@ -11,12 +11,12 @@ export async function middleware(request: NextRequest) {
         (
             url.pathname.startsWith('/sign-in') ||
             url.pathname.startsWith('/sign-up') ||
-            url.pathname.startsWith('/') ||
-            url.pathname.startsWith('/verify')
-        )){
+            url.pathname.startsWith('/verify') ||
+            url.pathname.startsWith('/')
+        ))
+        {
             return NextResponse.redirect(new URL('/dashboard', request.url))   
-        }
-  return NextResponse.redirect(new URL('/home', request.url))
+    }
 }
  
 // See "Matching Paths" below to learn more
